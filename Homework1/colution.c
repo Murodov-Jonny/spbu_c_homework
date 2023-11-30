958eecb550ccca5a0e864924b44a1edc3eb85dd9
Джонни <126473447+Murodov-Jonny@users.noreply.github.com>
2023-11-30 23:12:17 +0300

    Update colution.c

---
+++ Homework1/colution.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() {
+    int a, b;
+
+    printf("Введите значение для a: ");
+    scanf("%d", &a);
+
+    printf("Введите значение для b: ");
+    scanf("%d", &b);
+
+    printf("До обмена:\n");
+    printf("a = %d\n", a);
+    printf("b = %d\n", b);
+
+    // Обмен значениями без использования третьей переменной
+    a = a ^ b;
+    b = a ^ b;
+    a = a ^ b;
+
+    printf("После обмена:\n");
+    printf("a = %d\n", a);
+    printf("b = %d\n", b);
+
+
+    return 0;
+}
